dafd6dc2533a52718d03101ea02683f752c9ee14
Janne Romppanen <janneromppanen@fastmail.com>
2019-01-16 09:44:05 +0200

    frontend build lisatty

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.static('build')) // use static files from frontend build
 
 /**
  * middleware for allowing cross-origin resource sharing.
